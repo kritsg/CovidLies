c9251c355dc38bc3110e6aaa136e92d39fe6be57
Theodor Har <theodorlehar@gmail.com>
2021-05-22 13:45:20 -0700

    simple naive bayes(incomplete)

---
+++ train.py
@@ -0,0 +1,29 @@
+from sklearn.naive_bayes import ComplementNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+training = pd.read_csv('covid_lies.csv')
+test = pd.read_csv('covid_lies.csv')
+training.drop('tweet_id')
+test.drop('tweet_id')
+#Training/Test sets
+xtrain = training.drop('label', axis=1)
+ytrain = training.loc[:, 'label']
+xtest = test.drop('label', axis=1)
+ytest = test.loc[:, 'label']
+
+model = ComplementNB()
+model.fit(xtrain, ytrain)
+pred = model.predict(xtest)
+
+#Plot Confusion Matrix
+mat = confusion_matrix(pred, ytest)
+names = np.unique(pred)
+sns.heatmap(mat, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=names, yticklabels=names)
+plt.xlabel('Truth')
+plt.ylabel('Predicted')
