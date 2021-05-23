51b3277777500e559b65864370cd43f5f69b5e6b
Jeein Kim <51982182+lockpicks@users.noreply.github.com>
2021-05-22 22:24:43 -0700

    naive bayes splitting method changed

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,7 @@
 from sklearn.naive_bayes import ComplementNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,6 +12,7 @@ import texthero as hero
 training = pd.read_csv('covid_lies.csv')
 training.drop('misconception',axis = 1, inplace=True)
 training.drop('tweet_id',axis = 1,inplace = True)
+Y = training['label'] # get the label here
 training['tweet'] = hero.clean(training['tweet'])
 num_features = 500
 training['tweet'] = (hero.do_tfidf(training['tweet'], max_features=500))
@@ -18,13 +20,17 @@ training['tweet'] = (hero.do_tfidf(training['tweet'], max_features=500))
 tweets_df = pd.DataFrame(training["tweet"].to_list(), columns=['tweet_' + str(x) for x in range(num_features)])
 training.drop('tweet',axis = 1, inplace = True)
 training = pd.concat([training,tweets_df],axis = 1)
-print(training)
-test = training.copy()
-#Training/Test sets
-xtrain = training.drop('label', axis=1)
-ytrain = training.loc[:, 'label']
-xtest = test.drop('label', axis=1)
-ytest = test.loc[:, 'label']
+# print(training)
+
+training = training.drop('label', axis=1)
+
+# print(training)
+
+# split data into train and test sets
+seed = 7
+test_size = 0.3
+xtrain, xtest, ytrain, ytest = train_test_split(training, Y, test_size=test_size, random_state=seed)
+
 
 model = ComplementNB()
 model.fit(xtrain, ytrain)
